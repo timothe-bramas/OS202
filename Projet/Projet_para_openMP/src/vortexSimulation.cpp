ac69016fd1e1370a8827afffd08e93650f1d72a9
wootoonooyoo <sphericalpenguins@gmail.com>
2023-03-17 23:10:04 +0100

    added cloud slicing functionality

--- Projet/Projet_para_openMP/src/vortexSimulation.cpp
+++ Projet/Projet_para_openMP/src/vortexSimulation.cpp
@@ -7,15 +7,16 @@
 #include <string>
 #include <tuple>
 #include <chrono>
+#include <mpi.h>
 #include "cartesian_grid_of_speed.hpp"
 #include "vortex.hpp"
 #include "cloud_of_points.hpp"
 #include "runge_kutta.hpp"
 #include "screen.hpp"
 
-auto readConfigFile(std::ifstream &input)
+auto readConfigFile( std::ifstream& input , int nbProcessors, int sliceNo)
 {
-    using point = Simulation::Vortices::point;
+    using point=Simulation::Vortices::point;
 
     int isMobile;
     std::size_t nbVortices;
@@ -25,44 +26,58 @@ auto readConfigFile(std::ifstream &input)
     char buffer[maxBuffer];
     std::string sbuffer;
     std::stringstream ibuffer;
+
     // Lit la première ligne de commentaire :
     input.getline(buffer, maxBuffer); // Relit un commentaire
-    input.getline(buffer, maxBuffer); // Lecture de la grille cartésienne
-    sbuffer = std::string(buffer, maxBuffer);
+    input.getline(buffer, maxBuffer);// Lecture de la grille cartésienne
+    sbuffer = std::string(buffer,maxBuffer);
     ibuffer = std::stringstream(sbuffer);
     double xleft, ybot, h;
     std::size_t nx, ny;
     ibuffer >> xleft >> ybot >> nx >> ny >> h;
-    cartesianGrid = Numeric::CartesianGridOfSpeed({nx, ny}, point{xleft, ybot}, h);
+    cartesianGrid = Numeric::CartesianGridOfSpeed({nx,ny}, point{xleft,ybot}, h);
     input.getline(buffer, maxBuffer); // Relit un commentaire
     input.getline(buffer, maxBuffer); // Lit mode de génération des particules
-    sbuffer = std::string(buffer, maxBuffer);
+    sbuffer = std::string(buffer,maxBuffer);
     ibuffer = std::stringstream(sbuffer);
     int modeGeneration;
     ibuffer >> modeGeneration;
-    if (modeGeneration == 0) // Génération sur toute la grille
+    if (modeGeneration == 0) // Génération sur toute la grille 
     {
         std::size_t nbPoints;
         ibuffer >> nbPoints;
         cloudOfPoints = Geometry::generatePointsIn(nbPoints, {cartesianGrid.getLeftBottomVertex(), cartesianGrid.getRightTopVertex()});
     }
-    else
+    else 
     {
         std::size_t nbPoints;
         double xl, xr, yb, yt;
         ibuffer >> xl >> yb >> xr >> yt >> nbPoints;
-        cloudOfPoints = Geometry::generatePointsIn(nbPoints, {point{xl, yb}, point{xr, yt}});
+
+        // Calculate the width
+        double width = static_cast<double> (nx) * h;
+
+        // Divide according to processor number
+        nbPoints = nbPoints / nbProcessors;
+
+        // Calculate the bottom-left bound
+        xl = -50 + width * sliceNo / nbProcessors;
+        yb = -50;
+
+        // Calculate the top-right bound
+        xr = -50 + width * (sliceNo + 1) / nbProcessors;
+        yt = 50;
+
+        cloudOfPoints = Geometry::generatePointsIn(nbPoints, {point{xl,yb}, point{xr,yt}});
     }
     // Lit le nombre de vortex :
     input.getline(buffer, maxBuffer); // Relit un commentaire
     input.getline(buffer, maxBuffer); // Lit le nombre de vortex
     sbuffer = std::string(buffer, maxBuffer);
     ibuffer = std::stringstream(sbuffer);
-    try
-    {
-        ibuffer >> nbVortices;
-    }
-    catch (std::ios_base::failure &err)
+    try {
+        ibuffer >> nbVortices;        
+    } catch(std::ios_base::failure& err)
     {
         std::cout << "Error " << err.what() << " found" << std::endl;
         std::cout << "Read line : " << sbuffer << std::endl;
@@ -70,19 +85,19 @@ auto readConfigFile(std::ifstream &input)
     }
     Simulation::Vortices vortices(nbVortices, {cartesianGrid.getLeftBottomVertex(),
                                                cartesianGrid.getRightTopVertex()});
-    input.getline(buffer, maxBuffer); // Relit un commentaire
-    for (std::size_t iVortex = 0; iVortex < nbVortices; ++iVortex)
+    input.getline(buffer, maxBuffer);// Relit un commentaire
+    for (std::size_t iVortex=0; iVortex<nbVortices; ++iVortex)
     {
         input.getline(buffer, maxBuffer);
-        double x, y, force;
+        double x,y,force;
         std::string sbuffer(buffer, maxBuffer);
         std::stringstream ibuffer(sbuffer);
         ibuffer >> x >> y >> force;
-        vortices.setVortex(iVortex, point{x, y}, force);
+        vortices.setVortex(iVortex, point{x,y}, force);
     }
-    input.getline(buffer, maxBuffer); // Relit un commentaire
-    input.getline(buffer, maxBuffer); // Lit le mode de déplacement des vortex
-    sbuffer = std::string(buffer, maxBuffer);
+    input.getline(buffer, maxBuffer);// Relit un commentaire
+    input.getline(buffer, maxBuffer);// Lit le mode de déplacement des vortex
+    sbuffer = std::string(buffer,maxBuffer);
     ibuffer = std::stringstream(sbuffer);
     ibuffer >> isMobile;
     return std::make_tuple(vortices, isMobile, cartesianGrid, cloudOfPoints);
@@ -90,13 +105,28 @@ auto readConfigFile(std::ifstream &input)
 
 int main(int nargs, char *argv[])
 {
+
+    int rank, nbp;
+    MPI_Comm globcom;
+    MPI_Init(&nargs, &argv);
+    MPI_Comm_dup(MPI_COMM_WORLD, &globcom);
+    MPI_Comm_size(globcom, &nbp);
+    MPI_Comm_rank(globcom, &rank);
+    MPI_Status status;
+    MPI_Request request;
+
+    if (nbp != 2)
+    {
+        std::cout << "There must have 2 processes on this program";
+        return EXIT_FAILURE;
+    }
+
     char const *filename;
     if (nargs == 1)
     {
         std::cout << "Usage : vortexsimulator <nom fichier configuration>" << std::endl;
         return EXIT_FAILURE;
     }
-
     filename = argv[1];
     std::ifstream fich(filename);
     auto config = readConfigFile(fich);
@@ -108,84 +138,139 @@ int main(int nargs, char *argv[])
         resx = std::stoull(argv[2]);
         resy = std::stoull(argv[3]);
     }
-
     auto vortices = std::get<0>(config);
     auto isMobile = std::get<1>(config);
     auto grid = std::get<2>(config);
     auto cloud = std::get<3>(config);
-
-    std::cout << "######## Vortex simultor ########" << std::endl
-              << std::endl;
-    std::cout << "Press P for play animation " << std::endl;
-    std::cout << "Press S to stop animation" << std::endl;
-    std::cout << "Press right cursor to advance step by step in time" << std::endl;
-    std::cout << "Press down cursor to halve the time step" << std::endl;
-    std::cout << "Press up cursor to double the time step" << std::endl;
-
     grid.updateVelocityField(vortices);
 
-    Graphisme::Screen myScreen({resx, resy}, {grid.getLeftBottomVertex(), grid.getRightTopVertex()});
-    bool animate = false;
-    double dt = 0.1;
+    double again = 1; // il servira pour dire au process 1 jusqu'à quand calculer
 
-    while (myScreen.isOpen())
+    if (rank == 0)
     {
-        auto start = std::chrono::system_clock::now();
+        std::cout << "######## Vortex simultor ########" << std::endl
+                  << std::endl;
+        std::cout << "Press P for play animation " << std::endl;
+        std::cout << "Press S to stop animation" << std::endl;
+        std::cout << "Press right cursor to advance step by step in time" << std::endl;
+        std::cout << "Press down cursor to halve the time step" << std::endl;
+        std::cout << "Press up cursor to double the time step" << std::endl;
+
+        Graphisme::Screen myScreen({resx, resy}, {grid.getLeftBottomVertex(), grid.getRightTopVertex()});
+        double animate = 0;
+        double dt = 0.1;
+        double advance = 0;
+        double tab[4] = {animate,
+                         advance,
+                         again,
+                         dt};
+
+        bool entry = false;
+        while (myScreen.isOpen())
+        {
+            entry = 0;
+            auto start = std::chrono::system_clock::now();
+            advance = false;
+            // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+            sf::Event event;
+            while (myScreen.pollEvent(event))
+            {
+                // évènement "fermeture demandée" : on ferme la fenêtre
+                if (event.type == sf::Event::Closed)
+                {
+                    myScreen.close();
+                    tab[2] = 0;
+                    entry = true;
+                }
+                if (event.type == sf::Event::Resized)
+                {
+                    // on met à jour la vue, avec la nouvelle taille de la fenêtre
+                    myScreen.resize(event);
+                }
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
+                {
+                    animate = 1;
+                    entry = true;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                {
+                    animate = 0;
+                    entry = true;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+                {
+                    dt *= 2;
+                    entry = true;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+                {
+                    dt /= 2;
+                    entry = true;
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+                {
+                    advance = 1;
+                    entry = true;
+                }
+                tab[0] = animate;
+                tab[1] = advance;
+                tab[3] = dt;
+            }
+            if (entry)
+                MPI_Isend(tab, 4, MPI_DOUBLE, 1, 1234, globcom, &request);
+
+            MPI_Recv((double *)cloud.m_setOfPoints.data(), 2 * cloud.numberOfPoints(), MPI_DOUBLE, 1, 1111, globcom, &status);
+
+            myScreen.clear(sf::Color::Black);
+            std::string strDt = std::string("Time step : ") + std::to_string(dt);
+            myScreen.drawText(strDt, Geometry::Point<double>{50, double(myScreen.getGeometry().second - 96)});
+            myScreen.displayVelocityField(grid, vortices);
+            myScreen.displayParticles(grid, vortices, cloud);
+            auto end = std::chrono::system_clock::now();
+            std::chrono::duration<double> diff = end - start;
+            std::string str_fps = std::string("FPS : ") + std::to_string(1. / diff.count());
+            myScreen.drawText(str_fps, Geometry::Point<double>{300, double(myScreen.getGeometry().second - 96)});
+            myScreen.display();
+        }
+    } 
+    else    
+    {
+        bool animate = false;
         bool advance = false;
-        // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
-        sf::Event event;
-        while (myScreen.pollEvent(event))
+        double dt = 0.1;
+        double *tabloc = (double *)malloc(4 * sizeof(double));
+        int flag = 0;
+        while (again == 1)
         {
-            // évènement "fermeture demandée" : on ferme la fenêtre
-            if (event.type == sf::Event::Closed)
-                myScreen.close();
-            if (event.type == sf::Event::Resized)
+            MPI_Iprobe(0, 1234, globcom, &flag, &status);
+            if (flag == 1)
             {
-                // on met à jour la vue, avec la nouvelle taille de la fenêtre
-                myScreen.resize(event);
+                MPI_Recv(tabloc, 4, MPI_DOUBLE, 0, 1234, globcom, &status);
+                animate = (bool)tabloc[0];
+                advance = (bool)tabloc[1];
+                again = tabloc[2];
+                dt = tabloc[3];
             }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
-                animate = true;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-                animate = false;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-                dt *= 2;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-                dt /= 2;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-                advance = true;
-        }
-        if (animate | advance)
-        {
-            auto startcalc = std::chrono::system_clock::now();
+            flag = 0;
 
-            if (isMobile)
+            if (animate | advance)
             {
-                cloud = Numeric::solve_RK4_movable_vortices(dt, grid, vortices, cloud);
+                if (isMobile)
+                {
+                    cloud = Numeric::solve_RK4_movable_vortices(dt, grid, vortices, cloud);
+                }
+                else
+                {
+
+                    cloud = Numeric::solve_RK4_fixed_vortices(dt, grid, cloud);
+                }
             }
-            else
-            {
-                cloud = Numeric::solve_RK4_fixed_vortices(dt, grid, cloud);
-            }
-            auto endcalc = std::chrono::system_clock::now();
-            std::chrono::duration<double> diffcalc = endcalc - startcalc;
-            // std::cout << "Temps de calcul : " << std::to_string(diffcalc.count()) << "sec" << std::endl;
+            if (again == 1)
+                MPI_Send((double *)cloud.m_setOfPoints.data(), 2 * cloud.numberOfPoints(), MPI_DOUBLE, 0, 1111, globcom);
         }
-        auto start_aff = std::chrono::system_clock::now();
-        myScreen.clear(sf::Color::Black);
-        std::string strDt = std::string("Time step : ") + std::to_string(dt);
-        myScreen.drawText(strDt, Geometry::Point<double>{50, double(myScreen.getGeometry().second - 96)});
-        myScreen.displayVelocityField(grid, vortices);
-        myScreen.displayParticles(grid, vortices, cloud);
-        auto end = std::chrono::system_clock::now();
-        auto end_aff = std::chrono::system_clock::now();
-        std::chrono::duration<double> diffaff = end_aff - start_aff;
-        // std::cout << "Temps d'affichage : " << std::to_string(diffaff.count()) << "sec" << std::endl;
-        std::chrono::duration<double> diff = end - start;
-        std::string str_fps = std::string("FPS : ") + std::to_string(1. / diff.count());
-        myScreen.drawText(str_fps, Geometry::Point<double>{300, double(myScreen.getGeometry().second - 96)});
-        myScreen.display();
+        free(tabloc);
     }
-
+    MPI_Finalize();
     return EXIT_SUCCESS;
 }
